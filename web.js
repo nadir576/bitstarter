70813a8e1cfe5b60c947d30d70cc92ae3f44e358
nadir576 <nadir_ali@hotmail.com>
2013-07-11 23:41:36 -0400

    updated web.js reading from index.html

--- web.js
+++ web.js
@@ -2,13 +2,16 @@ var fs = require("fs");
 var filesname = "index.html"
 
 var express = require('express');
+var data = fsreadFileSync(filesname,"utf8");
+var buffer = new Buffer(data.size)
+	var data1 = buffer.toString(buffer);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var data = buffer.toString(fs.readFileSync(filename,"utf8"));
 
-  response.send(data);
+
+  response.send(data1);
 });
 
 var port = process.env.PORT || 5000;
